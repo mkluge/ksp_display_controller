62753ba9c1237507f498eba8ba01b833965ff7b4
mkluge <michael.kluge@tu-dresden.de>
2017-06-19 10:27:22 +0200

    flexible display functions

--- src/display_controller.cpp
+++ src/display_controller.cpp
@@ -6,8 +6,18 @@
 #include <SPI.h>
 #include "Ucglib.h"
 
-Ucglib_ST7735_18x128x160_SWSPI lcd_right( 1, 2, 3, 0, 4);
-Ucglib_ST7735_18x128x160_SWSPI lcd_left( 9, 10, 11, 8, 12);
+#define DisplayType Ucglib_ST7735_18x128x160_SWSPI
+
+#define CHECK_DATA(KEY,LCD,LINE) \
+		if ( (index=check_for_key( data, KEY))!=KEY_NOT_FOUND ) { \
+			print_lcd( LCD, LINE, (const char *) data[index+1]); }
+
+#define CHECK_BAR(KEY,LCD,INDEX) \
+					if ( (index=check_for_key( data, KEY))!=KEY_NOT_FOUND ) { \
+						setBarPercentage( LCD, INDEX, atoi(data[index+1])); }
+
+DisplayType lcd_right( 1, 2, 3, 0, 4);
+DisplayType lcd_left( 9, 10, 11, 8, 12);
 
 #define READ_BUFFER_SIZE 200
 char read_buffer[READ_BUFFER_SIZE];
@@ -16,20 +26,124 @@ int empty_buffer_size = 0;
 volatile bool have_handshake=false;
 volatile bool command_complete=false;
 
+#define BAR_FIRST_VERTICAL_OFFSET 5
+#define BAR_INNER_WIDTH 100
+#define BAR_INNER_HEIGHT 10
+#define BAR_BORDER_DISTANCE 4
+#define BAR_BORDER_RADIUS 2
+#define BAR_LEFT_OFFSET 5
+#define BAR_VERTICAL_DISTANCE 15
+#define BAR_TEXTV_ADD 10
+#define BAR_BORDER_HEIGHT (BAR_INNER_HEIGHT+2*BAR_BORDER_DISTANCE)
+#define BAR_TOTAL_HEIGHT (BAR_VERTICAL_DISTANCE+BAR_BORDER_HEIGHT)
+#define BAR_TOTAL_WIDTH (BAR_INNER_WIDTH+2*BAR_BORDER_DISTANCE)
+
 void receiveEvent(int how_many);
 void reset_input_buffer();
+signed int check_for_key( JsonArray &data, short key);
 
-void setupLCD( Ucglib_ST7735_18x128x160_SWSPI &lcd) {
-	  lcd.begin(UCG_FONT_MODE_SOLID);
+void print_lcd( DisplayType &lcd, int line, const char *str);
+void print_lcd( DisplayType &lcd, int line, int col, const char c);
+void print_lcd( DisplayType &lcd, int line, int number);
+
+void prepareFuelBar( DisplayType &lcd);
+void updateFuelBar(DisplayType &lcd, JsonArray& data);
+
+void prepareLanding( DisplayType &lcd);
+void updateLanding(DisplayType &lcd, JsonArray& data);
+
+void setBarPercentage( DisplayType &lcd, int number, float percentage);
+
+typedef struct LCDMode {
+	void (*initFunc)(DisplayType &lcd);
+	void (*updateFunc)(DisplayType &lcd, JsonArray& data);
+	LCDMode *next;
+} LCDModeS;
+
+LCDModeS fuel = { prepareFuelBar, updateFuelBar, NULL };
+LCDModeS landing = { prepareLanding, updateLanding, &fuel };
+LCDModeS *current_lcd_mode = &fuel;
+
+void setupLCD( DisplayType &lcd) {
+	  lcd.begin(UCG_FONT_MODE_TRANSPARENT);
 	  lcd.setRotate180();
 	  lcd.setColor(0, 255, 255, 255);
 	  lcd.setColor(1, 0, 0, 0);
+		lcd.setFont(ucg_font_9x15_mf);
 	  lcd.clearScreen();
 }
 
+void initBar( DisplayType &lcd, int number, const char *text)
+{
+	int border_t = BAR_FIRST_VERTICAL_OFFSET+number*BAR_TOTAL_HEIGHT;
+  int border_l = BAR_LEFT_OFFSET;
+
+	lcd.drawRFrame( border_l, border_t, BAR_TOTAL_WIDTH, BAR_TOTAL_HEIGHT, BAR_BORDER_RADIUS);
+	lcd.setPrintPos( border_l+BAR_BORDER_DISTANCE, border_t+BAR_TOTAL_HEIGHT+BAR_TEXTV_ADD);
+	lcd.print(text);
+}
+
+void prepareLanding( DisplayType &lcd)
+{
+	lcd.clearScreen();
+	print_lcd( lcd, 0, "Surf. Height");
+	print_lcd( lcd, 2, "Surf. Time");
+}
+
+void updateLanding(DisplayType &lcd, JsonArray& data)
+{
+	signed index = KEY_NOT_FOUND;
+	CHECK_DATA( INFO_SURFACE_HEIGHT, lcd, 1);
+	CHECK_DATA( INFO_SURFACE_TIME, lcd, 3);
+}
+
+void prepareFuelBar( DisplayType &lcd)
+{
+	lcd.clearScreen();
+	initBar( lcd, 0, "Liquid Fuel");
+	initBar( lcd, 1, "Oxygen");
+	initBar( lcd, 2, "RCS Fuel");
+	initBar( lcd, 3, "Battery");
+}
+
+void updateFuelBar(DisplayType &lcd, JsonArray& data)
+{
+	signed index=KEY_NOT_FOUND;
+	CHECK_BAR( INFO_PERCENTAGE_FUEL, lcd, 0 );
+	CHECK_BAR( INFO_PERCENTAGE_OXYGEN, lcd, 1 );
+	CHECK_BAR( INFO_PERCENTAGE_RCS, lcd, 2 );
+	CHECK_BAR( INFO_PERCENTAGE_BATTERY, lcd, 3 );
+}
+
+void setBarPercentage( DisplayType &lcd, int number, float percentage)
+{
+	int border_t = BAR_FIRST_VERTICAL_OFFSET+number*BAR_TOTAL_HEIGHT;
+  int border_l = BAR_LEFT_OFFSET;
+	int bar_width = (int)(BAR_INNER_WIDTH*percentage/100.0);
+
+	// need to draw the color box as well as the black box
+	float other_brightness = percentage*255.0/100.0;
+	lcd.setColor( 255, other_brightness, (int) other_brightness);
+	lcd.drawBox( border_l+BAR_BORDER_DISTANCE,
+		           border_t+BAR_BORDER_DISTANCE,
+		           bar_width,
+							 BAR_INNER_HEIGHT);
+	// need to draw the color box as well as the black box
+	lcd.setColor( 0, 0, 0);
+	lcd.drawBox( border_l+BAR_BORDER_DISTANCE+bar_width,
+		 	         border_t+BAR_BORDER_DISTANCE,
+						 	 BAR_INNER_WIDTH-bar_width,
+							 BAR_INNER_HEIGHT);
+}
+
 void setup() {
+
+	fuel.next = &landing;
+
 	setupLCD( lcd_right );
 	setupLCD( lcd_left );
+
+	current_lcd_mode->initFunc( lcd_left );
 
 	reset_input_buffer();
 	Wire.onReceive(receiveEvent);
@@ -37,8 +151,8 @@ void setup() {
 	delay(100);
 }
 
-void print_lcd( Ucglib_ST7735_18x128x160_SWSPI &lcd, int line, const char *str) {
-	  lcd.setFont(ucg_font_9x15_mf);
+void print_lcd( DisplayType &lcd, int line, const char *str) {
+//	  lcd.setFont(ucg_font_9x15_mf);
 	  lcd.setPrintPos(1,20+line*18);
 	  char buf[20]="                   ";
 	  snprintf( buf, 15, "%13s", str);
@@ -46,14 +160,14 @@ void print_lcd( Ucglib_ST7735_18x128x160_SWSPI &lcd, int line, const char *str) 
 	  lcd.print(buf);
 }
 
-void print_lcd( Ucglib_ST7735_18x128x160_SWSPI &lcd, int line,
+void print_lcd( DisplayType &lcd, int line,
 		        int col, const char c) {
-	  lcd.setFont(ucg_font_helvR10_hr);
+//	  lcd.setFont(ucg_font_helvR10_hr);
 	  lcd.setPrintPos(1+10*col,20+line*15);
 	  lcd.print(c);
 }
 
-void print_lcd( Ucglib_ST7735_18x128x160_SWSPI &lcd, int line, int number) {
+void print_lcd( DisplayType &lcd, int line, int number) {
 	char buf[20];
 	sprintf( buf, "%d", number);
 	print_lcd( lcd, line, buf);
@@ -106,10 +220,6 @@ signed int check_for_key( JsonArray &data, short key)
 	return KEY_NOT_FOUND;
 }
 
-#define CHECK_DATA(KEY,LCD,LINE) \
-		if ( (index=check_for_key( data, KEY))!=KEY_NOT_FOUND ) { \
-			print_lcd( LCD, LINE, (const char *) data[index+1]); }
-
 void work_on_command(JsonObject& rj) {
 	if (!rj.success()) {
 		print_lcd( lcd_left, 4, read_buffer);
@@ -119,8 +229,6 @@ void work_on_command(JsonObject& rj) {
 		{
 			if (rj["start"] == 2016) {
 				have_handshake = true;
-				print_lcd( lcd_left, 0, "Surf. Height");
-				print_lcd( lcd_left, 2, "Surf. Time");
 				print_lcd( lcd_right, 0, "Apoapsis");
 				print_lcd( lcd_right, 2, "time to AP");
 				print_lcd( lcd_right, 4, "Periapsis");
@@ -136,8 +244,12 @@ void work_on_command(JsonObject& rj) {
 			CHECK_DATA( INFO_APOAPSIS_TIME, lcd_right, 3);
 			CHECK_DATA( INFO_PERIAPSIS, lcd_right, 5);
 			CHECK_DATA( INFO_APOAPSIS_TIME, lcd_right, 7);
-			CHECK_DATA( INFO_SURFACE_HEIGHT, lcd_left, 1);
-			CHECK_DATA( INFO_SURFACE_TIME, lcd_left, 3);
+			if( (index=check_for_key( data, BUTTON_NEXT_LEFT_LCD_MODE))!=KEY_NOT_FOUND )
+			{
+				current_lcd_mode = current_lcd_mode->next;
+				current_lcd_mode->initFunc( lcd_left );
+			}
+			current_lcd_mode->updateFunc( lcd_left, data);
 		}
 	}
 }
